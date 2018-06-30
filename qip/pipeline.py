d7819bbb460745d3fc401ff00c088c18fc9bc6c8
sumnernh@gmail.com <sumnernh@gmail.com>
2018-06-29 19:46:56 -0700

    Made pipeline into a generic run_graph call which uses a graph accumulator to process nodes.

--- qip/pipeline.py
+++ qip/pipeline.py
@@ -5,7 +5,7 @@ from qip.util import flatten
 
 def run(*args, state=None, feed=None, statetype=numpy.complex128, debug=False, strict=False, **kwargs):
     """
-    Runs pipeline using all qubits in *args
+    Runs pipeline using all qubits in *args. Produces an output state based on input.
     :param args: list of qubits to evaluate
     :param state: full state of n qbits
     :param feed: feed of individual qbit states
@@ -71,6 +71,12 @@ def run(*args, state=None, feed=None, statetype=numpy.complex128, debug=False, s
 
 
 def get_deps(*args, feed=None):
+    """
+    Gets all graph nodes required for evaluation of graph.
+    :param args: desired output nodes.
+    :param feed: nodes with preassigned values.
+    :return: list of top level nodes and list of all nodes.
+    """
     if feed is None:
         feed = {}
 
@@ -86,54 +92,6 @@ def get_deps(*args, feed=None):
             frontier.update(node.inputs)
         seen.add(node)
     return list(deps), seen
-
-
-def feed_forward(frontier, state, graphnodes, statetype=numpy.complex128, debug=False):
-    # Condition is that if a node is in the frontier, either all its inputs are
-    # in the feed dict, or it itself is.
-    qbitindex = {}
-    seen = set()
-    n = 0
-    for qbit in sorted(frontier, key=lambda q: q.qid):
-        qbitindex[qbit] = [i for i in range(n, n+qbit.n)]
-        n += qbit.n
-
-    if len(state) != 2**n:
-        raise ValueError("Size of state must be 2**n")
-
-    state = state.astype(statetype)
-    arena = numpy.ndarray(shape=(2**n,), dtype=statetype)
-    classic_map = {}
-
-    while len(frontier) > 0:
-        frontier = list(sorted(frontier, key=lambda q: q.qid))
-        node, frontier = frontier[0], frontier[1:]
-
-        if debug:
-            print(node)
-
-        state, arena, (n_bits, bits) = node.feed(state, qbitindex, n, arena)
-
-        # Manage measurements
-        qbitindex = node.remap_index(qbitindex, n)
-
-        if n_bits > 0 or bits is not None:
-            classic_map[node] = bits
-            n -= n_bits
-
-        seen.add(node)
-        # Iterate sink for special cases where "cloning" takes place (i.e. splitting qubits after operation)
-        for nextnode in node.sink:
-            if nextnode in graphnodes and nextnode not in seen and nextnode not in frontier:
-                all_deps = True
-                for prevnode in nextnode.inputs:
-                    if prevnode not in seen:
-                        all_deps = False
-                        break
-                if all_deps:
-                    frontier.append(nextnode)
-                    qbitindex[nextnode] = nextnode.select_index(flatten(qbitindex[j] for j in nextnode.inputs))
-    return state, classic_map
 
 
 def make_circuit_mat(*args):
@@ -153,66 +111,28 @@ def make_circuit_mat(*args):
         mat[:,i] = o
     return mat
 
-blacklist = ["SplitQubit", "Q"]
-def printCircuit(*args, opwidth=1, linespacing=1, outputfn=print):
-    """
-    Print out circuit which leads to qubits in args
-    :param args: list of qubits (same as would be passed to run(*args)
-    """
-    frontier, graphnodes = get_deps(*args)
-    frontier = list(sorted(frontier, key=lambda q: q.qid))
 
+def run_graph(frontier, graphnodes, graphacc):
+    """
+    Apply the feed function from graphacc to each node in the graph in the order necessary to run the circuit.
+    :param frontier: top level nodes of graph
+    :param graphnodes: all nodes in graph
+    :param graphacc: graph accumulator class with "feed(qbitindex, node) -> void" function.
+    """
+    # Condition is that if a node is in the frontier, either all its inputs are
+    # in the feed dict, or it itself is.
     qbitindex = {}
     seen = set()
     n = 0
-    for qbit in frontier:
+    for qbit in sorted(frontier, key=lambda q: q.qid):
         qbitindex[qbit] = [i for i in range(n, n + qbit.n)]
         n += qbit.n
 
-    qubit_line = (" "*opwidth) + "|" + (" "*opwidth)
     while len(frontier) > 0:
         frontier = list(sorted(frontier, key=lambda q: q.qid))
         node, frontier = frontier[0], frontier[1:]
 
-        indices = flatten([qbitindex[qubit] for qubit in node.inputs])
-        if len(indices) > 0:
-            nodestr = repr(node)
-            paren_pos = nodestr.find('(')
-            if paren_pos > 0:
-                nodestr = nodestr[:paren_pos]
-
-            if nodestr not in blacklist:
-                # print node at relevant positions
-                default_line = [qubit_line] * n
-
-                outputfn((" " * linespacing).join(default_line))
-
-                for index in indices:
-                    default_line[index] = "-"*(2*opwidth + 1)
-                outputfn((" "*linespacing).join(default_line))
-
-                for nodechr in nodestr:
-                    default_line = [qubit_line]*n
-                    for index in indices:
-                        default_line[index] = "|" + (" "*(opwidth-1)) + nodechr + (" "*(opwidth-1)) + "|"
-                    outputfn((" " * linespacing).join(default_line))
-
-                max_len = max(len(str(i)) for i in range(len(indices)))
-                index_strs = []
-                for i in range(len(indices)):
-                    index_str = str(i)
-                    difflen = max_len - len(index_str)
-                    if difflen > 0:
-                        index_str = " "*difflen + index_str
-                    index_strs.append(index_str)
-                for l in range(max_len):
-                    for i,index in enumerate(indices):
-                        default_line[index] = "|" + (" " * (opwidth - 1)) + index_strs[i][l] + (" " * (opwidth - 1)) + "|"
-                    outputfn((" " * linespacing).join(default_line))
-
-                for index in indices:
-                    default_line[index] = "-"*(2*opwidth + 1)
-                outputfn((" "*linespacing).join(default_line))
+        graphacc.feed(qbitindex, node)
 
         # Manage measurements
         qbitindex = node.remap_index(qbitindex, n)
@@ -229,3 +149,88 @@ def printCircuit(*args, opwidth=1, linespacing=1, outputfn=print):
                 if all_deps:
                     frontier.append(nextnode)
                     qbitindex[nextnode] = nextnode.select_index(flatten(qbitindex[j] for j in nextnode.inputs))
+
+
+class NodeFeeder:
+    def __init__(self, state, n, statetype=numpy.complex128):
+        self.state = state.astype(statetype)
+        self.arena = numpy.ndarray(shape=(2 ** n,), dtype=statetype)
+        self.n = n
+        self.classic_map = {}
+
+        if len(self.state) != 2 ** n:
+            raise ValueError("Size of state must be 2**n")
+
+    def feed(self, qbitindex, node):
+        self.state, self.arena, (n_bits, bits) = node.feed(self.state, qbitindex, self.n, self.arena)
+
+        if n_bits > 0 or bits is not None:
+            self.classic_map[node] = bits
+            self.n -= n_bits
+
+
+def feed_forward(frontier, state, graphnodes, statetype=numpy.complex128, debug=False):
+    n = sum(f.n for f in frontier)
+    graphacc = NodeFeeder(state, n, statetype)
+    run_graph(frontier, graphnodes, graphacc)
+    return graphacc.state, graphacc.classic_map
+
+
+blacklist = ["SplitQubit", "Q"]
+class PrintFeeder:
+    def __init__(self, n, opwidth=1, linespacing=1, outputfn=print):
+        self.opwidth = opwidth
+        self.linespacing = linespacing
+        self.outputfn = outputfn
+        self.n = n
+        self.qubit_line = (" "*self.opwidth) + "|" + (" "*self.opwidth)
+
+    def feed(self, qbitindex, node):
+        indices = flatten([qbitindex[qubit] for qubit in node.inputs])
+        if len(indices) > 0:
+            nodestr = repr(node)
+            paren_pos = nodestr.find('(')
+            if paren_pos > 0:
+                nodestr = nodestr[:paren_pos]
+
+            if nodestr not in blacklist:
+                # print node at relevant positions
+                default_line = [self.qubit_line] * self.n
+
+                self.outputfn((" " * self.linespacing).join(default_line))
+
+                for index in indices:
+                    default_line[index] = "-" * (2 * self.opwidth + 1)
+                self.outputfn((" " * self.linespacing).join(default_line))
+
+                for nodechr in nodestr:
+                    default_line = [self.qubit_line] * self.n
+                    for index in indices:
+                        default_line[index] = "|" + (" " * (self.opwidth - 1)) + nodechr + (" " * (self.opwidth - 1)) + "|"
+                    self.outputfn((" " * self.linespacing).join(default_line))
+
+                max_len = max(len(str(i)) for i in range(len(indices)))
+                index_strs = []
+                for i in range(len(indices)):
+                    index_str = str(i)
+                    difflen = max_len - len(index_str)
+                    if difflen > 0:
+                        index_str = " " * difflen + index_str
+                    index_strs.append(index_str)
+                for l in range(max_len):
+                    for i, index in enumerate(indices):
+                        default_line[index] = "|" + (" " * (self.opwidth - 1)) + index_strs[i][l] + (
+                                    " " * (self.opwidth - 1)) + "|"
+                    self.outputfn((" " * self.linespacing).join(default_line))
+
+                for index in indices:
+                    default_line[index] = "-" * (2 * self.opwidth + 1)
+                self.outputfn((" " * self.linespacing).join(default_line))
+
+
+def printCircuit(*args, opwidth=1, linespacing=1, outputfn=print):
+    frontier, graphnodes = get_deps(*args)
+    frontier = list(sorted(frontier, key=lambda q: q.qid))
+    n = sum(f.n for f in frontier)
+    graphacc = PrintFeeder(n, opwidth, linespacing, outputfn)
+    run_graph(frontier, graphnodes, graphacc)
