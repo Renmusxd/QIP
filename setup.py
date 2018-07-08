c7edb287c8cfe2cdfa26ed89d8619a8efab28d94
sumnernh@gmail.com <sumnernh@gmail.com>
2018-07-08 13:44:23 -0700

    Fixed missing requirements in setup.py file.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='QIP',
-    version='0.2.3',
+    version='0.2.4',
     description='Quantum Computing Library',
     long_description=readme,
     author='Sumner Hearth',
@@ -21,5 +21,6 @@ setup(
     packages=find_packages(exclude=('tests','benchmark')),
     ext_modules=cythonize([
         Extension("qip.ext.*", ["qip/ext/*.pyx"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include()])
-    ])
+    ]),
+    install_requires=['numpy>=1.13.1', 'scipy>=0.19.1', 'cython>=0.27.3']
 )
